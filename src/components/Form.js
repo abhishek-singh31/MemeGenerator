bc2dea101cb7e6b0f38e05da787655de230f5c05
Abhishek Pratap Singh <apsingh394@gmail.com>
2022-04-07 00:42:52 +0530

    Added Header & Form components

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+export default function Form(){
+    return (
+        <form>
+            <div className="input-div">
+                <input className="input1" placeholder="Enter upper text"></input>
+                <input className="input2" placeholder="Enter lower text"></input>
+            </div>
+            <input type="button" value="Get a new meme image 🖼️"/>
+        </form>
+    )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return (
+        <nav>
+            <div className="image-box">
+                <img className="meme" src="./images/troll-face.png" alt="meme"/>
+                <h3>Meme Generator</h3>
+            </div>
+            <p>React Course - Project 3</p>
+        </nav>
+    )
+}
