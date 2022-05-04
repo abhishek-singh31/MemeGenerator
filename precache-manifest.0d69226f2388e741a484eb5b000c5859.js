00fb1da8931ec01bbfde07876e6297cb3d5de33a
Abhishek Pratap Singh <apsingh394@gmail.com>
2022-05-05 00:58:43 +0530

    Updates

---
+++ precache-manifest.0d69226f2388e741a484eb5b000c5859.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca14962a01b0fa738f8a",
+    "url": "/MemeGenerator/static/js/main.a7e0f918.chunk.js"
+  },
+  {
+    "revision": "e8a27393b2bd3d3ed146",
+    "url": "/MemeGenerator/static/js/runtime~main.2cbc2ac9.js"
+  },
+  {
+    "revision": "b6e084e57cc3ecdc5990",
+    "url": "/MemeGenerator/static/js/2.fde1ea41.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/MemeGenerator/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "ac47bec6b59be027df0dc6808a86a2c9",
+    "url": "/MemeGenerator/index.html"
+  }
+];
